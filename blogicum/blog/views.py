9fc235529ae95a1d08269582d711ed7837c9ec0b
Данил Казаченко <ditussimulus@gmail.com>
2024-02-04 13:38:59 +0300

    Версия для тестов

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -46,17 +46,18 @@ posts = [
 
 def index(request):
     template = 'blog/index.html'
-    context = {'posts': posts}
+    context = {'posts': reversed(posts)}
     return render(request, template, context)
 
 
 def post_detail(request, id):
     template = 'blog/detail.html'
-    context = {'posts': posts[id]}
+    if posts:
+        context = {'posts': posts[id]}
     return render(request, template, context)
 
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
-    context =  category_slug
+    context =  {'slug': category_slug}
     return render(request, template, context)
